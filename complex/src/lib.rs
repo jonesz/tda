47009803a8123f7c74be21e32591bd449d270276
ethan jones <etn.jones@gmail.com>
2021-12-16 11:23:52 -0600

    R; Rename the existing simplex_trie.

--- complex/src/lib.rs
+++ complex/src/lib.rs
@@ -1,8 +1,8 @@
 // src/complex/src/lib.rs
 //! Fundamental utilities for computing and interacting with Simplices.
 mod simplex;
-mod simplex_trie;
 mod simplex_trie_arena;
+mod simplex_trie_ptr;
 pub mod vietoris_rips;
 
 pub struct SimplicialComplex(simplex_trie::SimplexTrie, usize);
