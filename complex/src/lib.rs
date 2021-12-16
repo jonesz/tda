96cbb44f47786cefa74b8cfd7c738502e65bc335
ethan jones <etn.jones@gmail.com>
2021-12-16 12:41:36 -0600

    R; initial creation of a trait for a SimplexTrie.

--- complex/src/lib.rs
+++ complex/src/lib.rs
@@ -1,6 +1,7 @@
 // src/complex/src/lib.rs
 //! Fundamental utilities for computing and interacting with Simplices.
 mod simplex;
+mod simplex_trie;
 mod simplex_trie_arena;
 mod simplex_trie_ptr;
 pub mod vietoris_rips;
---
+++ complex/src/simplex_trie.rs
@@ -0,0 +1,12 @@
+// src/complex/src/simplex_trie.rs
+use crate::simplex::Simplex;
+
+trait SimplexTrie {
+    type SimplexTrieIterator;
+
+    fn new() -> Self;
+    fn new_skel(sz: usize) -> Self;
+    fn add_simplex(&mut self, simplex: &Simplex);
+    fn contains_simplex(&mut self, simpleX: &Simplex) -> bool;
+    fn iter_dim(&self, sz: usize) -> Self::SimplexTrieIterator;
+}
